4e6c3879c0de0594338eba29d02b898b88726ed0
Brad Fitzpatrick <bradfitz@golang.org>
2016-08-09 18:22:40 +0000

    Add dummy Go package

---
+++ doc.go
@@ -0,0 +1,8 @@
+// Package goissuemirror is a dummy package with no exported
+// symbols, just so programs can depend on it and run "go get -u"
+// to update its data files.
+//
+// To use the data in this repo, see:
+//
+//    https://github.com/bradfitz/issuemirror/
+package goissuemirror
